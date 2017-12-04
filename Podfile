167d01c561df22b6ef889fa8827d10940f2971e7
LH <1911382216@qq.com>
2017-12-04 20:49:58 +0800

    创建仓库

---
+++ Podfile
@@ -0,0 +1,13 @@
+platform :ios, '8.0'
+
+target ‘mp’ do
+
+pod 'FMDB'
+pod 'AFNetworking', '~> 3.0'
+pod 'JSONModel'
+pod 'MBProgressHUD'
+pod ‘DKNightVersion’
+pod ’SDWebImage'
+pod 'YYText'
+pod 'MJRefresh'
+end
---
+++ mp/Main/MPAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mp
+//
+//  Created by admin on 2017/12/4.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
